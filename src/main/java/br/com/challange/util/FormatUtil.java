7d4e362d53c357a4889345cb7897e0fafb8fa744
douglas <darkyal.7@gmail.com>
2024-04-11 01:33:01 -0300

    feat: FormatUtil for Date

--- src/main/java/br/com/challange/util/FormatUtil.java
+++ src/main/java/br/com/challange/util/FormatUtil.java
@@ -1,7 +1,11 @@
 package br.com.challange.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class FormatUtil {
     public static String dateTimeNow(){
@@ -21,4 +25,20 @@ public class FormatUtil {
         LocalDateTime dateTimeNow = LocalDateTime.now();
         return dtf.format(dateTimeNow);
     }
+
+    public static Date getDateFromLocalDateTime(){
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        return Date.from(dateTimeNow.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date getOnlyDate() {
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return formatter.parse(formatter.format(new Date()));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
